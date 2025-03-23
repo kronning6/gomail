e143e3e25d9b82bd58d13119403f4a8ab15e7171
Kyle Ronning <kyleronning+kronning6@gmail.com>
2025-03-22 22:35:29 -0500

    Fix Use so subcommand works

--- cmd/starred.go
+++ cmd/starred.go
@@ -7,7 +7,7 @@ import (
 )
 
 var starredCmd = &cobra.Command{
-	Use:   "gomail starred",
+	Use:   "starred",
 	Short: "Show starred emails",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("This subcommand will show starred emails")
